ec738ba45b3b70af1d88d4270a0e0079988bf72e
FelipeMaciel05 <felimorita05@gmail.com>
2022-09-05 10:33:18 -0300

    perfil

---
+++ perfil.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfil</title>
+    <link rel="stylesheet" href="perfil.css">
+</head>
+<body>
+<div id = "container">
+            <header>
+                <h1> Ort Launcher</h1>
+                <div class="logo">
+                    <img src="logo terminado.png" width="150px">
+                </div>
+            </header>
+            
+            <nav>
+                <ul>
+                    <li>
+                        <a href= "http://localhost/ANTEPROYECTO/index.php">Inicio</a>
+                    </li>
+                    <li>
+                        <a href= "http://localhost/ANTEPROYECTO/juegosRecomendados.php">Juegos Recomendados</a>
+                    </li>
+                    <li>
+                        <a href= "http://localhost/ANTEPROYECTO/biblioteca.php">Biblioteca</a>
+                    </li>
+                    <li>
+                        <a href= "http://localhost/ANTEPROYECTO/perfil.php">Perfil</a>
+                    </li>
+                    <li>
+                        <a href= "http://localhost/ANTEPROYECTO/ayuda.php">Ayuda</a>
+                    </li>
+                    <li>
+                        <a href= "http://localhost/ANTEPROYECTO/publicarJuego.php">Publicar Juego</a>
+                    </li>
+                </ul>
+            </nav>
+        </div> 
+        <div class="foto">
+            <div class="perfil">
+                <img src="perfil-empty.png" width="400px">
+            </div>     
+        </div>
+
+
+        <footer>
+            <div class="contenedor">
+                <h2 class="titulo">Medios de contacto:</h2>
+            </div>
+            <div class="ig">
+                <img src="ig.png" width="50px">
+            </div>
+            <div class="fc">
+                <a href="https://www.wattpad.com/amp/837524206"> <img src="fc.png" width="50px"></a>
+            </div>
+            <div class="redit">
+                <img src="redit.png" width="52px">
+            </div>
+            <label class="copy">Ort Launcher © Todos los derechos reservados</label>
+            <label class="donar"><a href="http://localhost/ANTEPROYECTO/donaciones.php">Donaciones</a></label>
+            <label class="panas"><a href="nuestra informacion">Sobre Nosotros</a></label>
+      </footer>
+</body>
+</html>
